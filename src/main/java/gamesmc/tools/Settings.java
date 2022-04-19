5468929d2958c22b34ad696ee2931035bc795c63
ReferTV <HeCorepomoc@gmail.com>
2022-04-19 19:34:01 +0200

    Tests

--- src/main/java/gamesmc/tools/Settings.java
+++ src/main/java/gamesmc/tools/Settings.java
@@ -7,13 +7,23 @@ public class Settings extends YamlConfig {
     @Ignore
     public static final Settings IMP = new Settings();
 
-    public String PREFIX = "ayo";
+    public String PREFIX = "&cGames&fMC&e.pl &8»";
+
+    @Comment({
+            "Available serializers:",
+            "LEGACY_AMPERSAND - \"&c&lExample &c&9Text\".",
+            "LEGACY_SECTION - \"§c§lExample §c§9Text\".",
+            "MINIMESSAGE - \"<bold><red>Example</red> <blue>Text</blue></bold>\". (https://webui.adventure.kyori.net/)",
+            "GSON - \"[{\"text\":\"Example\",\"bold\":true,\"color\":\"red\"},{\"text\":\" \",\"bold\":true},{\"text\":\"Text\",\"bold\":true,\"color\":\"blue\"}]\". (https://minecraft.tools/en/json_text.php/)",
+            "GSON_COLOR_DOWNSAMPLING - Same as GSON, but uses downsampling."
+    })
+    public String SERIALIZER = "LEGACY_AMPERSAND";
 
     @Create
-    public MAIN MAIN;
+    public messages messages;
 
-    public static class MAIN {
+    public static class messages {
 
-        public String FIELD = "hi";
+        public String error_no_permission = "";
     }
 }
--- src/main/java/gamesmc/tools/Tools.java
+++ src/main/java/gamesmc/tools/Tools.java
@@ -1,19 +1,46 @@
 package gamesmc.tools;
 
+import gamesmc.tools.commands.tools;
+import net.elytrium.java.commons.mc.serialization.Serializer;
+import net.elytrium.java.commons.mc.serialization.Serializers;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.ComponentSerializer;
 import org.bukkit.plugin.java.JavaPlugin;
-import gamesmc.tools.Settings;
 
 import java.io.File;
+import java.util.Objects;
 
 public final class Tools extends JavaPlugin {
 
+    private static Serializer serializer;
 
     @Override
     public void onEnable() {
         Settings.IMP.reload(new File(this.getDataFolder(), "config.yml"), Settings.IMP.PREFIX);
+        this.getLogger().info("GamesMC Tools 2.0");
+        this.registerCommands();
+
+        ComponentSerializer<Component, Component, String> serializer = Serializers.valueOf(Settings.IMP.SERIALIZER).getSerializer();
+        if (serializer == null) {
+            this.getLogger().info("The specified serializer could not be founded, using default. (LEGACY_AMPERSAND)");
+            setSerializer(new Serializer(Objects.requireNonNull(Serializers.LEGACY_AMPERSAND.getSerializer())));
+        } else {
+            setSerializer(new Serializer(serializer));
+        }
+
+    }
+
+    private void registerCommands() {
+        this.getCommand("tools").setExecutor(new tools());
     }
 
     @Override
     public void onDisable() {
     }
+
+
+    private static void setSerializer(Serializer serializer) {
+
+    }
+
 }
